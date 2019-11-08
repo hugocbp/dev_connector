bdecbcda6e100e2abb1a0b0d844bba782f95edca
Hugo Carlos Borges Pinto <hugocbp@gmail.com>
2019-11-07 16:28:58 -0800

    Add validation for user creation

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+// Connect Database
 connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => res.send("API Running"));
 
