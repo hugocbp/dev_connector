664df86cebd472f4a9508b4f1e87509ec60a4ec4
Hugo Carlos Borges Pinto <hugocbp@gmail.com>
2019-11-11 17:17:47 -0800

    Add default and optional timeout for Alerts

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,7 +1,7 @@
 import { SET_ALERT, REMOVE_ALERT } from "./types";
 import uuid from "uuid";
 
-export const setAlert = (msg, alertType) => dispatch => {
+export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
   const id = uuid.v4();
 
   dispatch({
@@ -9,5 +9,5 @@ export const setAlert = (msg, alertType) => dispatch => {
     payload: { msg, alertType, id }
   });
 
-  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), 5000);
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
 };
